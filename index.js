60289704e26fbb0d145ea39b2e77be02a0cef6ac
Eduard Pikhno <i@emdc.ru>
2017-03-17 20:11:35 +0300

    fix changes, version 0.2.0

---
+++ index.js
@@ -0,0 +1,115 @@
+(function () {
+  Array.isArray = Array.isArray ? Array.isArray : function (arg) { return Object.prototype.toString.call(arg) === '[object Array]' }
+
+  var Bitte = function () {
+    this._get = {}
+    this._currentLocale = undefined
+    this._links = {}
+  }
+
+  Bitte.prototype.add = function () {
+    var localeName
+    var translation
+
+    if (arguments.length === 1) {
+      if (Array.isArray(arguments[0])) {
+        var scope = this
+        arguments[0].forEach(function (translationObj) {
+          if (!translationObj.locale || typeof translationObj.locale !== 'string') throw new Error('Invalid locale name')
+          if (typeof translationObj.data !== 'object') throw new Error('Invalid translation object')
+          scope._get[translationObj.locale] = translationObj.data
+        })
+      } else {
+        localeName = arguments[0]
+        translation = arguments[1]
+        if (typeof localeName !== 'string') throw new Error('Incorrect locale name')
+        if (typeof translation !== 'object') throw new Error('Incorrect translation object')
+        this._get[localeName] = translation || {}
+      }
+    } else if (arguments.length === 2) {
+      localeName = arguments[0]
+      translation = arguments[1]
+      if (!localeName || typeof localeName !== 'string') throw new Error('Invalid locale name')
+      if (typeof translation !== 'object') throw new Error('Invalid translation object')
+      this._get[localeName] = translation
+    }
+
+    if (!this._currentLocale) {
+      var localesList = Object.keys(this._get)
+      if (localesList.length) {
+        this._currentLocale = localesList[0]
+      }
+      return this
+    } else {
+      return this
+    }
+  }
+
+  Bitte.prototype.set = function (property, value, localeName) {
+    localeName = localeName || this._currentLocale
+    if (!localeName) throw new Error('Undefined or incorrect locale name')
+    if (!this._get[localeName]) throw new Error('No stored locale "' + localeName + '" for setting property')
+    if (typeof value !== 'string') throw new Error('Set property value from no string type')
+    this._get[localeName][property] = value
+  }
+
+  Bitte.prototype.get = function (property, localeName) {
+    if (!property) throw new Error('No property or it\'s invalid.')
+    localeName = localeName || this._currentLocale
+    if (!localeName) throw new Error('Undefined or incorrect locale name')
+
+    if (this._get[localeName]) {
+      if (this._get[localeName][property]) {
+        return this._get[localeName][property]
+      } else {
+        throw new Error('Can\'t find property "' + property + '" for locale "' + localeName + '".')
+      }
+    } else {
+      throw new Error('No translation "' + localeName + '" in storage.')
+    }
+  }
+
+  Bitte.prototype.locales = function () {
+    return Object.keys(this._get)
+  }
+
+  Bitte.prototype.currentLocale = function (locale) {
+    if (locale) {
+      if (!this._get[locale]) throw new Error('No locale with name "' + locale + '"')
+      this._currentLocale = locale
+      if (Object.keys(this._links).length) this._updateLinks()
+    } else {
+      return this._currentLocale
+    }
+  }
+
+  Bitte.prototype.link = function (item, property, setter) {
+    this._links[property] = this._links[property] || []
+    this._links[property].push({ item: item, setter: setter })
+  }
+
+  Bitte.prototype._updateLinks = function () {
+    var scope = this
+    Object.keys(scope._links).forEach(function (prop) {
+      if (scope._links[prop]) {
+        scope._links[prop].forEach(function (link) {
+          if (link.setter) {
+            link.setter(link.item, scope.get(prop))
+          } else {
+            if (window) {
+              link.item.textContent = scope.get(prop)
+            } else {
+              throw new Error('Can\'t apply changes to item because there is no setter function')
+            }
+          }
+        })
+      }
+    })
+  }
+
+  if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+    module.exports = Bitte
+  } else {
+    window.Bitte = Bitte
+  }
+})()
---
+++ test/bitte.test.js
@@ -0,0 +1,274 @@
+var assert = require('assert')
+var Bitte = require('../')
+
+describe('Bitte package', function () {
+  describe('Create object', function () {
+    it('Create object with no translation', function() {
+      var bitte = new Bitte()
+      assert.equal(undefined, bitte._currentLocale)
+
+      assert.deepEqual({}, bitte._get)
+    })
+
+    it('Error, when create object with translation. but without locale', function() {
+      var exceptionWasFound = false
+
+      try {
+        var bitte = new Bitte().add(null, { test: 'value'})
+      } catch (e) {
+        exceptionWasFound = true
+      }
+      
+      assert.ok(exceptionWasFound)
+    })
+
+    it('Create object with translate', function() {
+      var bitte = new Bitte().add('en-US', { test: 'value' })
+      assert.equal('en-US', bitte._currentLocale)
+      assert.equal('value', bitte._get['en-US'].test)
+    })
+
+    it('Create object with multiple translations', function () {
+      var bitte = new Bitte().add([
+        { locale: 'en-US', data: { test: 'value one' } },
+        { locale: 'de-DE', data: { test: 'value two' } }
+      ])
+
+      assert.equal('value one', bitte._get['en-US'].test)
+      assert.equal('value two', bitte._get['de-DE'].test)
+    })
+  })
+
+  describe('Setter', function () {
+    it('Successful setting of the property for default locale', function () {
+      var bitte = new Bitte().add('en-US', { })
+      bitte.set('key', 'value')
+
+      assert.equal('value', bitte._get['en-US']['key'])
+    })
+
+    it('Error when setting property for undefined default locale', function () {
+      var bitte = new Bitte()
+      var exceptionWasFound = false
+
+      try {
+        bitte.set('key', 'value')
+      } catch (e) {
+        exceptionWasFound = true
+      }
+      
+      assert.ok(exceptionWasFound)
+    })
+
+    it('Successful setting of the property for some locale, if it exist', function () {
+      var bitte = new Bitte().add('en-US', { })
+
+      try {
+        bitte.set('key', 'value', 'en-US')
+      } catch (e) {
+        assert.ok(false)
+      }
+      assert.equal('value', bitte._get['en-US']['key'])
+    })
+
+    it('Error when setting of the property for some locale, but it not exist', function () {
+      var bitte = new Bitte().add('en-US', { })
+      var exceptionWasFound = false
+
+      try {
+        bitte.set('key', 'value', 'ru-RU')
+      } catch (e) {
+        exceptionWasFound = true
+      }
+      assert.ok(exceptionWasFound)
+    })
+
+    it('Error when setting property with no-string type', function () {
+      var bitte = new Bitte().add('en-US', { })
+      var exceptionWasFound = false
+
+      try {
+        bitte.set('key', 10, 'en-US')
+      } catch (e) {
+        exceptionWasFound = true
+      }
+      assert.ok(exceptionWasFound)
+    })
+  })
+
+  describe('Getter', function () {
+    it('Successful getting property from default locale', function () {
+      var bitte = new Bitte().add('en-US', { key: 'value' })
+      assert.equal('value', bitte.get('key'))
+    })
+
+    it('Error when getting property with incorrect name', function () {
+      var bitte = new Bitte('en-US', { })
+      var undefinedFound = false
+      var nullFound = false
+      var nanFound = false
+      var numberFound = false
+      var objectFound = false
+
+      try {
+        var value = bitte.get()
+      } catch (e) {
+        undefinedFound = true
+      }
+
+      try {
+        var value = bitte.get(null)
+      } catch (e) {
+        nullFound = true
+      }
+
+      try {
+        var value = bitte.get(NaN)
+      } catch (e) {
+        nanFound = true
+      }
+
+      try {
+        var value = bitte.get(10)
+      } catch (e) {
+        numberFound = true
+      }
+
+      try {
+        var value = bitte.get({ prop: 10 })
+      } catch (e) {
+        objectFound = true
+      }
+
+      assert.ok(undefinedFound && nullFound && nanFound && numberFound && objectFound)
+    })
+
+    it('Error when no property in trnaslation', function () {
+      var bitte = new Bitte().add('en-US', { key: 'value' })
+      var exceptionFound = false
+      try {
+        var value = bitte.get('prop')
+      } catch (e) {
+        exceptionFound = true
+      }
+      assert.ok(exceptionFound)
+    })
+
+    it('Error when no default translation', function () {
+      var bitte = new Bitte()
+      var exceptionFound = false
+      try {
+        var value = bitte.get('prop')
+      } catch (e) {
+        exceptionFound = true
+      }
+      assert.ok(exceptionFound)
+    })
+
+    it('Error when no translation', function () {
+      var bitte = new Bitte().add('en-US', { key: 'value' })
+      var exceptionFound = false
+
+      try {
+        var value = bitte.get('prop', 'ru-RU')
+      } catch (e) {
+        exceptionFound = true
+      }
+      assert.ok(exceptionFound)
+    })
+  })
+
+  describe('Add new translation', function() {
+    it('Add incorrect translation gives error', function () {
+      var bitte = new Bitte()
+      var exceptionFound = false
+      try {
+        bitte.add('en-US', 'value')
+      } catch (e) {
+        exceptionFound = true
+      }
+      assert.ok(exceptionFound)
+    })
+
+    it('Add translation with inclorrect locale gives error', function () {
+      var bitte = new Bitte()
+      var exceptionFound = false
+      try {
+        bitte.add('en-US', 'value')
+      } catch (e) {
+        exceptionFound = true
+      }
+      assert.ok(exceptionFound)
+    })
+
+    it('Correctly add translations array', function () {
+      var bitte = new Bitte().add([ {locale: 'en-US', data: {}}, {locale: 'ru-RU', data: {}}])
+      assert.deepEqual(['en-US', 'ru-RU'], Object.keys(bitte._get))
+    })
+  })
+
+  describe('Set current locale', function () {
+    it('Default current locale is undefined', function () {
+      var bitte = new Bitte()
+      assert.equal(undefined, bitte._currentLocale)
+    })
+
+    it('Current locale after add translations is the first added locale', function () {
+      var bitte = new Bitte().add([ {locale: 'en-US', data: {}}, {locale: 'ru-RU', data: {}}])
+      assert.equal('en-US', bitte._currentLocale)
+    })
+
+    it('Correctly setting current locale if it exist', function () {
+      var bitte = new Bitte().add([ {locale: 'en-US', data: {}}, {locale: 'ru-RU', data: {}}])
+      bitte.currentLocale('ru-RU')
+      assert.equal('ru-RU', bitte._currentLocale)
+    })
+
+    it('Get current locale', function () {
+      var bitte = new Bitte().add([ {locale: 'en-US', data: {}}, {locale: 'ru-RU', data: {}}])
+      assert.equal('en-US', bitte.currentLocale())
+    })
+
+    it('Correct locales list', function () {
+      var bitte = new Bitte().add([ {locale: 'en-US', data: {}}, {locale: 'ru-RU', data: {}}])
+      assert.deepEqual(['en-US', 'ru-RU'], bitte.locales())
+    })
+  })
+
+  var testTranslations = [
+    {locale: 'en-US', data: { test: 'value' }}, 
+    {locale: 'ru-RU', data: { test: 'prop' }}
+  ]
+  describe('Auto update registered properties', function () {
+    it('Register link', function () {
+      var bitte = new Bitte().add(testTranslations)
+      var someObject = {}
+      bitte.link(someObject, 'test')
+      assert.deepEqual([{ item: {}, setter: undefined }], bitte._links['test'])
+    })
+
+    it('Update link', function () {
+      var bitte = new Bitte().add(testTranslations)
+      var someObject = { text: ''}
+      bitte.link(someObject, 'test', function(item, value) {
+        item.text = value
+      })
+      bitte.currentLocale('ru-RU')
+      assert.equal('prop', someObject.text)
+    })
+
+    it('If link is no DOM and no setter for this, throw err', function () {
+      var bitte = new Bitte().add(testTranslations)
+      var exceptionWasFound = false
+      var someObject = { text: ''}
+      bitte.link(someObject, 'test')
+      try {
+        bitte.currentLocale('ru-RU')
+      } catch (e) {
+        exceptionWasFound = true
+      }
+      
+      assert.ok(exceptionWasFound)
+    })
+  })
+})
